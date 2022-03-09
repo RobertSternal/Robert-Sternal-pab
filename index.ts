f7a3f33ae07bdfcb6bcd513bd60dd841abfa0921
Robert Sternal <robert.sternal@microsoft.wsei.edu.pl>
2022-03-09 11:31:17 +0100

    09.03 v2

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 const express = require('express')
  const app = express()
   app.get('/', function (req, res) { res.send('Hello World') })
-  app.get()
-
    app.listen(3000)
+
+app.get('/', function (req, bdd) { bdd.send("Kalkulator" )})
 
    
